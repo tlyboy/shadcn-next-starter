9bb0bd6abb2d2b1e451d2d366d6e309b03675e8f
Guany <1084072690@qq.com>
2025-12-01 15:59:07 +0800

    fix: update path mapping in tsconfig.json to point to app directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { ThemeProvider } from './components/theme-provider'
-import { ModeToggle } from './components/mode-toggle'
-import { Button } from './components/ui/button'
+import { ThemeProvider } from '@/components/theme-provider'
+import { ModeToggle } from '@/components/mode-toggle'
+import { Button } from '@/components/ui/button'
 
 export const metadata: Metadata = {
   title: 'shadcn next starter',
